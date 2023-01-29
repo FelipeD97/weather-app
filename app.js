e5dfbc47f29f6123285275682d27b83dbd67cc0e
Felipe Dunbar <felipe.a.dunbar@gmail.com>
2023-01-29 16:49:45 -0500

    Got weather API working. Working on zipcode/location converter using geocode api.

--- app.js
+++ app.js
@@ -1,24 +1,59 @@
+const apiKey = "88046bf7db800bafadd6f356173c3a84";
 
+let lat = 33.906991;
+let lon = -84.3686985;
 
-const apiKey = "ee3f96a31171485986b21238232501"
-const u = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Paris`
+// API URLs 
+const dailyWeatherLink = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+const weeklyWeatherLink = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+const geocodeAPI = `http://api.openweathermap.org/geo/1.0/direct?q=Atlanta&limit=1&appid=${apiKey}`;
 
-let get = (url) => {
+// Fetch URL Function 
+const get = async(url) => {
     return fetch(url)
-        .then(response => response.json())
-        .then(data => data);
-        
+        .then((response) => response.json());
 }
 
-let temp = get(u)
+// Convert Location to Lat-Long Coordinates for Weather API
 
-console.log(temp[1])
+const convertLocation = async() => {
+    let lat = 0
+    let coordinates = get(geocodeAPI).then(data => {
+        // console.log(data[0]);
+        let latitude = data[0].lat;
+        let longitude = data[0].lon;
+        let coordinates = [latitude, longitude]
+        return coordinates
+    });
+    coordinates.then(data => {console.log(data)})
+};
 
+convertLocation()
 
+// console.log(convertLocation().latitude)
+
+const todayWeather = document.querySelector("#weather-content");
+
+// Get Current Day Weather 
 const getWeather = () => {
+    get(dailyWeatherLink).then(data => {
+        console.log(data.main);
+        let temperature = document.createElement("h3");
+        temperature.innerText = data.main.temp;
+        todayWeather.append(temperature);
+    });
+};
 
-}
+// getWeather();
 
+// Get Weekly Forecast 
+// const getWeeklyForecast = () => {
+//     get(weeklyWeatherLink).then(data => {
+//         console.log(data);
+//     });
+// };
+
+// getWeeklyForecast();
 
 const temperatures = {
     hot: [],
---
+++ weather.js
@@ -0,0 +1,13 @@
+const weeklyWeather = {
+    main: {temp: 275.11, feels_like: 271.55, temp_min: 272.77, temp_max: 276.26, pressure: 1022},
+    weather: {
+        0: {
+            description: "clear sky",
+            icon: "01n",
+            id: 800,
+            main: "Clear"
+        }
+    }
+}
+
+
